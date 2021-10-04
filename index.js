8e9acf8c8c8d9e133f31c23a3442405004d066c4
nyanishant <nayanishant.info@gmail.com>
2021-10-05 01:50:10 +0530

    updated nav bar

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-const navSlide = () => {
-    const burger = document.querySelector('.hamburger'); 
-    const nav = document.querySelector('.nav-links');
-    const navlinks = document.querySelectorAll('.nav-links li');
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-    burger.addEventListener("click", () => {
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
-        //Animate Links 
-        navlinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
-    });
-}
-
-navSlide();
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
 
 var t1 = gsap.timeline();
 var query = window.matchMedia('(min-width: 992px)');
